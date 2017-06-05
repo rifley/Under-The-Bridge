026d74e383174445a468b8a0b45841b43d8bee7c
Riley Watts and Stormi Hoebelheinrich <riley@>
2017-06-05 16:39:03 -0700

    add details to new topic fragment

--- app/src/main/java/com/example/guest/underthebridge/ui/NewTopicFragment.java
+++ app/src/main/java/com/example/guest/underthebridge/ui/NewTopicFragment.java
@@ -2,10 +2,13 @@ package com.example.guest.underthebridge.ui;
 
 import android.app.DialogFragment;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ImageButton;
 
 import com.example.guest.underthebridge.R;
 
@@ -16,10 +19,11 @@ import butterknife.ButterKnife;
  * Created by Guest on 6/5/17.
  */
 
-public class NewTopicFragment extends DialogFragment {
+public class NewTopicFragment extends DialogFragment implements View.OnClickListener{
 
-   EditText mTopicEditText;
-   EditText mImageUrlEditText;
+    EditText mTopicEditText;
+    EditText mImageUrlEditText;
+    ImageButton mTrollTickleButton;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
@@ -27,6 +31,20 @@ public class NewTopicFragment extends DialogFragment {
         getDialog().setTitle("Topic Fragment");
         mTopicEditText = (EditText) rootView.findViewById(R.id.topicEditText);
         mImageUrlEditText = (EditText) rootView.findViewById(R.id.imageUrlEditText);
+        mTrollTickleButton = (ImageButton) rootView.findViewById(R.id.trollTickleButton);
+
+        mTrollTickleButton.setOnClickListener(this);
+
         return rootView;
+    }
+
+    @Override
+    public void onClick(View v) {
+        if(v == mTrollTickleButton) {
+            String topic = mTopicEditText.getText().toString();
+            String imageURL = mImageUrlEditText.getText().toString();
+            Log.v("in fragment", topic);
+            dismiss();
+        }
     }
 }
