9139ee92f0883d4e8f884175a28e900c16bfb250
Riley Watts and Stormi Hoebelheinrich <stormi@>
2017-06-05 15:28:11 -0700

    add member variables to NewTopicFragment

--- app/src/main/java/com/example/guest/underthebridge/ui/NewTopicFragment.java
+++ app/src/main/java/com/example/guest/underthebridge/ui/NewTopicFragment.java
@@ -5,18 +5,28 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.EditText;
 
 import com.example.guest.underthebridge.R;
+
+import butterknife.Bind;
+import butterknife.ButterKnife;
 
 /**
  * Created by Guest on 6/5/17.
  */
 
 public class NewTopicFragment extends DialogFragment {
+
+   EditText mTopicEditText;
+   EditText mImageUrlEditText;
+
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View rootView = inflater.inflate(R.layout.fragment_topic_form, container, false);
         getDialog().setTitle("Topic Fragment");
+        mTopicEditText = (EditText) rootView.findViewById(R.id.topicEditText);
+        mImageUrlEditText = (EditText) rootView.findViewById(R.id.imageUrlEditText);
         return rootView;
     }
 }
