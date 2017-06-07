769c49997a40f3cd4eaab6e1f88234c27adff326
Riley Watts and Stormi Hoebelheinrich <stormi@>
2017-06-07 16:32:30 -0700

    add topicId to topic object on instantiation

--- app/src/main/java/com/example/guest/underthebridge/ui/adapters/FirebaseTopicViewHolder.java
+++ app/src/main/java/com/example/guest/underthebridge/ui/adapters/FirebaseTopicViewHolder.java
@@ -11,6 +11,7 @@ import android.widget.TextView;
 import com.example.guest.underthebridge.R;
 import com.example.guest.underthebridge.ui.models.Topic;
 import com.example.guest.underthebridge.ui.ui.TopicDetailActivity;
+import com.google.firebase.database.ChildEventListener;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
@@ -50,6 +51,7 @@ public class FirebaseTopicViewHolder extends RecyclerView.ViewHolder implements 
         Log.d("testing", topic.getTitle());
 
 
+
         mTopicListTextView.setText(topic.getTitle());
 
         Picasso.with(mContext)
@@ -64,19 +66,45 @@ public class FirebaseTopicViewHolder extends RecyclerView.ViewHolder implements 
         final ArrayList<Topic> topics = new ArrayList<>();
         DatabaseReference ref = FirebaseDatabase.getInstance().getReference("topics");
         ref.addListenerForSingleValueEvent(new ValueEventListener() {
+
+
             @Override
             public void onDataChange(DataSnapshot dataSnapshot) {
                 for(DataSnapshot snapshot : dataSnapshot.getChildren()){
                     topics.add(snapshot.getValue(Topic.class));
                 }
 
+
                 int itemPosition = getLayoutPosition();
 
                 Intent intent = new Intent(mContext, TopicDetailActivity.class);
-                intent.putExtra("position", itemPosition + "");
+                intent.putExtra("position", itemPosition);
                 intent.putExtra("topics", Parcels.wrap(topics));
 
                 mContext.startActivity(intent);
+            }
+
+            @Override
+            public void onCancelled(DatabaseError databaseError) {
+
+            }
+        });
+        ref.addChildEventListener(new ChildEventListener() {
+            @Override
+            public void onChildAdded(DataSnapshot dataSnapshot, String previousChildKey) {
+                Log.d("Child Listener", dataSnapshot.getKey());
+            }
+            @Override
+            public void onChildMoved(DataSnapshot dataSnapshot, String prevChildKey) {}
+
+            @Override
+            public void onChildChanged(DataSnapshot dataSnapshot, String s) {
+
+            }
+
+            @Override
+            public void onChildRemoved(DataSnapshot dataSnapshot) {
+
             }
 
             @Override
